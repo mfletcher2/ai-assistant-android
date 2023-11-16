0628713b09cdbae8ae5c184fe18731a509b22e54
Max Fletcher <92177225+mfletcher2@users.noreply.github.com>
2023-11-15 22:50:01 -0800

    Add Google Custom Search API

---
+++ app/src/main/java/lol/max/assistantgpt/CseAPI.kt
@@ -0,0 +1,61 @@
+package lol.max.assistantgpt
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class CseAPI {
+    @JsonPropertyDescription("The query to search")
+    @JsonProperty(required = true)
+    lateinit var query: String
+    private val key: String = BuildConfig.GOOGLE_API_KEY
+    private val pseId: String = BuildConfig.GOOGLE_SEARCH_ID
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("https://customsearch.googleapis.com/").build()
+    private val googleCseService = retrofit.create(GoogleCseService::class.java)
+
+    fun doSearch(query: String): List<Result> {
+        val response = googleCseService.getSearch(
+            key,
+            pseId,
+            query
+        )
+
+        val search = response.execute().body()
+        return search?.items ?: listOf()
+    }
+}
+
+
+interface GoogleCseService {
+    @GET("customsearch/v1")
+    fun getSearch(
+        @Query("key") key: String,
+        @Query("cx") id: String,
+        @Query("q") query: String,
+        @Query("num") num: Int = 10
+    ): Call<Search>
+}
+
+data class Search(
+    val queries: Queries, val items: List<Result>
+)
+
+data class Queries(
+    val request: List<Request>
+)
+
+data class Request(
+    val title: String
+)
+
+data class Result(
+    val title: String,
+    val displayLink: String,
+    val snippet: String
+)
